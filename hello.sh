43922ddce89affbe5e370762847c2a2e1dc83980
Andy Pohl <andy.pohl@wisc.edu>
2017-02-03 13:12:14 -0600

    Example cleanup.

--- hello.sh
+++ hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# hello-chtc.sh
-# My very first CHTC job
+# hello.sh
+# My very first HTCondor job
 #
-echo "Hello CHTC from Job $1 running on `whoami`@`hostname`"
+echo "Hello HTCondor from Job $1 running on `whoami`@`hostname`"
